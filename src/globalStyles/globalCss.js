4bef24a1fa40a4a13e22db1bbb3ea481fe833049
Rhys <rbrooker105@gmail.com>
2018-07-25 11:49:53 +0800

    add styled components and eslint

---
+++ src/globalStyles/globalCss.js
@@ -0,0 +1,5 @@
+const globalCSS = `
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700');
+`;
+
+export default globalCSS;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { injectGlobal } from 'styled-components';
 import App from './App';
+import globalCSS from './globalStyle/globalCss';
 import registerServiceWorker from './registerServiceWorker';
+
+injectGlobal`${globalCSS}`;
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+/* eslint no-undef: "off", no-unused-expressions: "off" */
